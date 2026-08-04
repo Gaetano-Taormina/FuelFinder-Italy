dae25a1e65e49357365382567c5aece0337bc565
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-08-04 16:00:00 +0000

    feat(api): modularize backend routing and controllers

---
+++ server/repositories/stationRepository.js
@@ -0,0 +1,26 @@
+export class StationRepository {
+    constructor(db) {
+        this.db = db;
+    }
+
+    findStationsInBoundingBox(minLat, maxLat, minLng, maxLng, fuelType, serviceType) {
+        let serviceCondition = '';
+        if (serviceType === '1') serviceCondition = 'AND p.is_self = 1';
+        else if (serviceType === '0') serviceCondition = 'AND p.is_self = 0';
+
+        const sql = `
+            SELECT s.id, s.gestore as brand, s.bandiera, s.nome_impianto as name, s.indirizzo as address, 
+                   s.comune, s.provincia, s.latitudine as lat, s.longitudine as lng,
+                   p.prezzo as currentPrice, p.is_self as isSelf
+            FROM stations s
+            CROSS JOIN prices p ON s.id = p.id_impianto
+            WHERE s.latitudine BETWEEN ? AND ? 
+              AND s.longitudine BETWEEN ? AND ?
+              AND p.desc_carburante = ?
+              ${serviceCondition}
+        `;
+
+        const stmt = this.db.prepare(sql);
+        return stmt.all(minLat, maxLat, minLng, maxLng, fuelType);
+    }
+}
